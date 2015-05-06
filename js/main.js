8ce4f36ca4db0b2f37a3834c7ba8f3a04a2d9335
Neil Nagarsheth <nn452412@ohio.edu>
2015-05-06 03:32:35 +0000

    adding check-off feature, allowing users to signify an item is done

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
 	var listData = new listData();
 	var descriptionText = 'description';
-	var checkedText = 'checked'
+	var checkedText = 'checked';
 	var sizeText = 'listSize';
 
 	if(localStorage.getItem(descriptionText) && localStorage.getItem(checkedText) && localStorage.getItem(sizeText)){
@@ -19,7 +19,15 @@ $(document).ready(function(){
 		listData.checked = JSON.parse(localStorage.getItem(checkedText));
 		listData.size = parseInt(localStorage.getItem(sizeText));
 		for(var key in listData.description){
-			$(".shiftnav-content-wrap").append("<div class = 'list-item'><p hidden>" + key + "</p>" + "<div class = 'list-input' contenteditable>" + listData.description[key] + "</div><p hidden>" + key + "</p>" + "<div class = 'remove-item'>X</div></div>");
+      var checkText = "";
+      var checkMark = false;
+      var strikeThrough = "none";
+      if(listData.checked[key]){
+        checkText = '\u2713';
+        checkMark = true;
+        strikeThrough = "line-through";
+      }
+			$(".shiftnav-content-wrap").append("<div class = 'list-item'><p hidden>" + key + "</p>" + "<div class = 'list-input' contenteditable="+ !checkMark + " style='text-decoration:"+strikeThrough+";'>" + listData.description[key] + "</div><p hidden>" + key + "</p><div class = 'remove-item'>X</div><p hidden>" + (listData.size-1) + "</p><div class = 'checkbox-item'>" + checkText + "</div></div>");
 		}
 	}
 	else{
@@ -32,8 +40,8 @@ $(document).ready(function(){
 	$("#add-button").click(function(){
 		listData.size += 1;
 		listData.checked[listData.size - 1] = false;
-    	listData.description[listData.size - 1] = "";
-    	$(".shiftnav-content-wrap").append("<div class = 'list-item'><p hidden>" + (listData.size-1) + "</p>" + "<div class = 'list-input' contenteditable>" + "</div><p hidden>" + (listData.size-1) + "</p>" + "<div class = 'remove-item'>X</div></div>");
+    listData.description[listData.size - 1] = "";
+    $(".shiftnav-content-wrap").append("<div class = 'list-item'><p hidden>" + (listData.size-1) + "</p>" + "<div class = 'list-input' contenteditable>" + "</div><p hidden>" + (listData.size-1) + "</p><div class = 'remove-item'>X</div><p hidden>" + (listData.size-1) + "</p><div class = 'checkbox-item'></div></div>");
 		localStorage.setItem(descriptionText, JSON.stringify(listData.description));
 		localStorage.setItem(checkedText, JSON.stringify(listData.checked));
 		localStorage.setItem(sizeText,listData.size);
@@ -48,7 +56,6 @@ $(document).ready(function(){
 	$(document).on("keyup change",'.list-input',function(){
 		listData.description[$(this).prev().text()] = $(this).text();
 		localStorage.setItem(descriptionText, JSON.stringify(listData.description));
-		localStorage.setItem(checkedText, JSON.stringify(listData.checked));
 	});
 
 	$(document).on("click",".remove-item",function(){
@@ -58,5 +65,21 @@ $(document).ready(function(){
 		localStorage.setItem(descriptionText, JSON.stringify(listData.description));
 		localStorage.setItem(checkedText, JSON.stringify(listData.checked));
 	});
+  
+  $(document).on("click",".checkbox-item",function(){
+    var checkText = "";
+    var checkMark = false;
+    var strikeThrough = "none";
+    if(!listData.checked[$(this).prev().text()]){
+      checkText = "\u2713";
+      checkMark = true;
+      strikeThrough = "line-through";
+    }
+    $(this).text(checkText);
+    $(this).prevAll('div.list-input').css("text-decoration",strikeThrough);
+    $(this).prevAll('div.list-input').attr('contentEditable',!checkMark);
+    listData.checked[$(this).prev().text()] = checkMark;
+    localStorage.setItem(checkedText, JSON.stringify(listData.checked));
+  });
 
 });
